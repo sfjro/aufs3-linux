68ce11f49ce7e3efbe463d82d499fa914a978fc1
J. R. Okajima <hooanon05g@gmail.com>
2015-03-24 03:04:30 +0900

    aufs: tiny, fold a long line
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/dcsub.h
+++ fs/aufs/dcsub.h
@@ -118,7 +118,8 @@ static inline int au_qstreq(struct qstr *a, struct qstr *b)
 
 /*
  * by the commit
- * 360f547 2015-01-25 dcache: let the dentry count go down to zero without taking d_lock
+ * 360f547 2015-01-25 dcache: let the dentry count go down to zero without
+ *			taking d_lock
  * the type of d_lockref.count became int, but the inlined function d_count()
  * still returns unsigned int.
  * I don't know why. Maybe it is for every d_count() users?
